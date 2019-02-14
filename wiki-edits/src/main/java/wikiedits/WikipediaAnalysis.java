7374880db80b715bf01ff3a7451a02087ebfd333
afuyo <artmro@gmail.com>
2019-02-14 10:36:26 +0100

    kafka output

--- wiki-edits/src/main/java/wikiedits/WikipediaAnalysis.java
+++ wiki-edits/src/main/java/wikiedits/WikipediaAnalysis.java
@@ -1,12 +1,18 @@
 package wikiedits;
 
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditEvent;
 import org.apache.flink.streaming.connectors.wikiedits.WikipediaEditsSource;
 
@@ -34,7 +40,15 @@ public class WikipediaAnalysis {
                     }
                 });
 
-        result.print();
+        //result.print();
+        result
+                .map(new MapFunction<Tuple2<String,Long>, String>() {
+                    @Override
+                    public String map(Tuple2<String, Long> tuple) {
+                        return tuple.toString();
+                    }
+                })
+                .addSink(new FlinkKafkaProducer011<>("localhost:9092", "wiki-result", new SimpleStringSchema()));
 
         see.execute();
     }
